3125b08074e455d46250a1a0d11de8a1e312c729
AshkanArabim <ashkan.arabim@gmail.com>
2025-12-20 16:19:07 -0700

    add docLoop

--- backend/main.go
+++ backend/main.go
@@ -9,20 +9,23 @@ import (
 	"github.com/coder/websocket"
 )
 
-type wsHandler struct{} // implements the Handler interface
 type userReadLoopResult struct {
 	mt  websocket.MessageType
 	b   []byte
 	err error
 }
+type userChangeAndConnType struct {
+	authorConn *websocket.Conn
+	bytes      []byte // cuz func (*SyncState) ReceiveMessage receives bytes
+}
 
-func WSReadLoop(ctx context.Context, conn *websocket.Conn, readClientChanges chan userReadLoopResult) {
-	defer close(readClientChanges)
+func wsReadLoop(ctx context.Context, conn *websocket.Conn, readUserChanges chan userReadLoopResult) {
+	defer close(readUserChanges)
 
 	// loop to read continuously
 	for {
 		mt, b, err := conn.Read(ctx)
-		readClientChanges <- userReadLoopResult{mt, b, err}
+		readUserChanges <- userReadLoopResult{mt, b, err}
 
 		if err != nil {
 			fmt.Println(err.Error())
@@ -32,77 +35,100 @@ func WSReadLoop(ctx context.Context, conn *websocket.Conn, readClientChanges cha
 }
 
 // each connected user has an instance of this func running
-func HandleUserConn(conn *websocket.Conn, serverChanges chan *automerge.SyncMessage, clientChanges chan *automerge.SyncMessage) {
-	defer conn.CloseNow()
-	defer close(serverChanges)
-	defer close(clientChanges)
-
+func userLoop(conn *websocket.Conn, serverChanges chan *automerge.SyncMessage, userChangesAndConn chan userChangeAndConnType) {
 	// FIXME: handle deliberate disconnection
+	// FIXME: close the connections / channels passed here from the owner process
 
 	// TODO: figure out a better context solution
 	ctx := context.Background()
-	readClientChanges := make(chan userReadLoopResult, 5)
-	go WSReadLoop(ctx, conn, readClientChanges)
+	readUserChanges := make(chan userReadLoopResult, 5)
+	go wsReadLoop(ctx, conn, readUserChanges)
 
 	for {
 		select {
 		case serverChange := <-serverChanges:
-			// send broadcast changes sent by the server; send to client
+			// send broadcast changes sent by the server; send to user
 			conn.Write(ctx, websocket.MessageType(websocket.MessageBinary), serverChange.Bytes())
 
-		case readClientChange := <-readClientChanges:
-			// client sent changes to server; forward to doc handler
-			b := readClientChange.b
-			err := readClientChange.err
+		case readUserChange := <-readUserChanges:
+			// user sent changes to server; forward to doc handler
+			b := readUserChange.b
+			err := readUserChange.err
 			if err != nil {
 				fmt.Println(err.Error())
 				return
 			}
 
-			// decode & send syncmessage to doc handler
-			clientChange, err := automerge.LoadSyncMessage(b)
-			if err != nil {
-				fmt.Println(err.Error())
-				return
-			}
-			clientChanges <- clientChange
+			// send syncmessage to doc handler
+			userChangesAndConn <- userChangeAndConnType{conn, b}
 		}
 	}
 }
 
-// handles each new user connection to a document
-// TODO: convert this to be a new instance for every new document.
+// every document has an instance of this func running.
+// will also launch instances of userLoop for every user that connects to this doc
+func docLoop(joiningConns chan *websocket.Conn, leavingConns chan *websocket.Conn) {
+	// set up automerge server
+	// since all in memory, starting with blank doc & syncstate
+	// TODO: add persistence here
+	doc := automerge.New()
+	syncstate := automerge.NewSyncState(doc)
+	userChangesAndConn := make(chan userChangeAndConnType, 10)
+	defer close(userChangesAndConn)
+
+	// loop for adding / removing users & merging / broadcasting changes
+	wsConnInputs := make(map[*websocket.Conn]chan *automerge.SyncMessage)
+	for {
+		select {
+		case conn := <-joiningConns:
+			wsConnInputs[conn] = make(chan *automerge.SyncMessage, 10)
+			go userLoop(conn, wsConnInputs[conn], userChangesAndConn)
+
+		case conn := <-leavingConns:
+			close(wsConnInputs[conn])
+			delete(wsConnInputs, conn)
+
+		case userChangeAndConn := <-userChangesAndConn:
+			// merge changes into doc
+			syncstate.ReceiveMessage(userChangeAndConn.bytes)
+
+			// broadcast changes to everyone except the author
+			serverChange, _ := syncstate.GenerateMessage()
+			for conn, ch := range wsConnInputs {
+				if conn != userChangeAndConn.authorConn {
+					ch <- serverChange
+				}
+			}
+		}
+	}
+}
+
 // FIXME: handle the case when HandleUserConn returns. TBD if both failure or also clean close
-func (s *wsHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+// accepts user WS connection and starts goroutine to send / receive changes
+func acceptUserConnection(w http.ResponseWriter, r *http.Request) {
 	// upgrade to websocket
 	conn, err := websocket.Accept(w, r, nil)
 	if err != nil {
 		fmt.Println(err.Error())
 		return
 	}
-	defer conn.CloseNow()
+	// FIXME: figure out when to close the connection
 
-	// set up automerge server
-	// since all in memory, starting with blank doc & syncstate
-	doc := automerge.New()
-	syncstate := automerge.NewSyncState(doc)
-
-  // launch user connection handler goroutine
-  serverChanges := make(chan *automerge.SyncMessage, 5)
-  clientChanges := make(chan *automerge.SyncMessage, 5)
-  go HandleUserConn(conn, serverChanges, clientChanges)
+	// launch user connection handler goroutine
+	// TODO:
 
 }
 
 func main() {
 	// set up server
-	const origin = ":8080"
+	const addr = ":8080"
 
+	mux := http.NewServeMux() // mux pointer
 	s := &http.Server{
-		Addr:    origin + "/document/0", // hardcoding doc id for now
-		Handler: &wsHandler{},
+		Addr:    addr,
+		Handler: mux,
 	}
 
-	fmt.Printf("Listening on %s...", origin)
+	fmt.Printf("Listening on %s...", addr)
 	s.ListenAndServe()
 }
